ddde340a665478251c6ceea7249e606be7153b36
KaiserinDerWelt <desjmarianaguilar@gmail.com>
2020-11-11 20:43:12 -0500

    Search

--- js/searchEngine.js
+++ js/searchEngine.js
@@ -1,11 +1,11 @@
 function functionSearch() {
-    var inputSearch, filterSearch, cardsSearch, cardContainer, mySearch, i;
-    inputSearch = document.getElementById("searchFilter"); //Input search
+    var inputSearch, filterSearch, cardsSearch, cardContainer, mySearch, i;//OK
+    inputSearch = document.getElementById("searchFilter"); //Input search //OK
     filterSearch = inputSearch.value.toUpperCase();
-    cardContainer = document.getElementById("items");//Container d
-    cardsSearch = cardContainer.getElementsByClassName("card");//Targeting Card
+    cardContainer = document.getElementById("items");//Container d//OK
+    cardsSearch = cardContainer.getElementsByClassName("card");//Targeting Card //OK
     for (i = 0; i < cardsSearch.length; i++) {
-        mySearch = cardsSearch[i].querySelector(".card-body");
+        mySearch = cardsSearch[i].querySelector(".row");
         if (mySearch.innerText.toUpperCase().indexOf(inputSearch) > -1) {
             cardsSearch[i].style.display = "";
         } else {
