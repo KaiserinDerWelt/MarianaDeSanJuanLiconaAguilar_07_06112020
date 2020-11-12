3f5c0aa1968e5d848ff43d8e4a10a2328b9ab179
KaiserinDerWelt <desjmarianaguilar@gmail.com>
2020-11-12 11:47:20 -0500

    Implemented and Tested Positive JSON Fetch API

--- js/fetch.js
+++ js/fetch.js
@@ -1,10 +1,26 @@
-fetch('#')
-.then(function (response) {
-  return response.json();//Getting Json set as parameter
-})
-.then(function (data) {
-  console.log(data); //It will append the data to HTML
-});
+document.getElementById("magButton"),addEventListener('click', loadJSON);
 
 
-//I'm experimenting some browser security restrictions to run the API
+function loadJSON(){
+  fetch('https://kaiserinderwelt.github.io/MarianaDeSanJuanLiconaAguilar_07_06112020/recipes.json')
+  .then(function (response) {
+    return response.json();//Getting Json set as parameter
+  })
+  .then(function (data) {
+    appendData(data); //It will append the data to HTML
+  })
+    .catch(function (err) {
+      console.log(err);
+  });
+}
+
+function appendData(data){
+  var mainContainer = document.getElementById("myData");
+            for (var i = 0; i < data.length; i++) {
+                var div = document.createElement("div");
+                div.innerHTML = 'Name: ' + data[i].name + ' ' + data[i].servings;
+                mainContainer.appendChild(div);
+}
+}
+
+
