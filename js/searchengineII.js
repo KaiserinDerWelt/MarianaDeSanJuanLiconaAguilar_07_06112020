a4a4f1e72376cc570f7fdf75d29e565c4d7aa304
KaiserinDerWelt <desjmarianaguilar@gmail.com>
2020-11-27 10:40:19 -0500

    Working on second branch

---
+++ js/searchengineII.js
@@ -0,0 +1,8 @@
+function functionSearch() {
+    var inputSearch, filterSearch, cardsSearch, cardContainer, mySearch, i;//OK
+    inputSearch = document.getElementById("searchFilter"); //Input search //OK
+    filterSearch = inputSearch.value.toUpperCase();
+    cardContainer = document.getElementById("items");//Container d//OK
+    cardsSearch = cardContainer.getElementsByClassName("card");//Targeting Card //
+    // Loop to search here
+}
